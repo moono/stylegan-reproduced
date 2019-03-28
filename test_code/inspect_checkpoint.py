d28036e37e8e0198e2ceb81af21b0ff277c5beb4
moono <toilety@gmail.com>
2019-03-28 14:46:41 +0900

    apply directory paths

--- test_code/inspect_checkpoint.py
+++ test_code/inspect_checkpoint.py
@@ -4,7 +4,7 @@ from tensorflow.python.tools import inspect_checkpoint as chkp
 
 
 def main():
-    chpt_fn = os.path.join('./official-pretrained', 'model.ckpt')
+    chpt_fn = os.path.join('../official-pretrained', 'model.ckpt')
 
     # print all tensors in checkpoint file
     chkp.print_tensors_in_checkpoint_file(chpt_fn, tensor_name='', all_tensors=False)
